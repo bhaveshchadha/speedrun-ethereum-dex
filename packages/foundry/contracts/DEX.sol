65fa08db51b533ebae1c33b353e723a1e9739492
bhaveshchadha <bhaveshchadha14@gmail.com>
2026-07-11 00:22:52 +0530

    Checkpoint 5

--- packages/foundry/contracts/DEX.sol
+++ packages/foundry/contracts/DEX.sol
@@ -9,19 +9,51 @@ contract DEX {
     /////////////////
 
     // Errors go here
+    error DexAlreadyInitialized();
+    error TokenTransferFailed();
+    error InvalidEthAmount();
+    error InvalidTokenAmount();
+    error InsufficientTokenBalance(uint256 available, uint256 required);
+    error InsufficientTokenAllowance(uint256 available, uint256 required);
+    error EthTransferFailed(address to, uint256 amount);
+    error InsufficientLiquidity(uint256 available, uint256 required);
 
     //////////////////////
     /// State Variables //
     //////////////////////
 
     IERC20 public immutable token;
+    uint256 public totalLiquidity;
+    mapping(address => uint256) liquidity;
 
     ////////////////
     /// Events /////
     ////////////////
 
     // Events go here...
+    event EthToTokenSwap(
+        address swapper,
+        uint256 tokenOutput,
+        uint256 ethInput
+    );
+    event TokenToEthSwap(
+        address swapper,
+        uint256 tokensInput,
+        uint256 ethOutput
+    );
 
+    event LiquidityProvided(
+        address liquidityProvider,
+        uint256 liquidityMinted,
+        uint256 ethInput,
+        uint256 tokensInput
+    );
+    event LiquidityRemoved(
+        address liquidityRemover,
+        uint256 liquidityWithdrawn,
+        uint256 tokensOutput,
+        uint256 ethOutput
+    );
     ///////////////////
     /// Constructor ///
     ///////////////////
@@ -34,31 +66,112 @@ contract DEX {
     /// Functions /////
     ///////////////////
 
-    function init(uint256 tokens) public payable returns (uint256 initialLiquidity) {
-        // Your code here...
+    function init(
+        uint256 tokens
+    ) public payable returns (uint256 initialLiquidity) {
+        if (totalLiquidity != 0) revert DexAlreadyInitialized();
+        totalLiquidity = msg.value;
+        liquidity[msg.sender] = totalLiquidity;
+        initialLiquidity = totalLiquidity;
+        (bool success) = token.transferFrom(msg.sender, address(this), tokens);
+        if (!success) revert TokenTransferFailed();
     }
 
-    function price(uint256 xInput, uint256 xReserves, uint256 yReserves) public pure returns (uint256 yOutput) {
+    function price(
+        uint256 xInput,
+        uint256 xReserves,
+        uint256 yReserves
+    ) public pure returns (uint256 yOutput) {
         // Your code here...
+        uint256 xInputWithFee = (xInput * 997) / 1000; //txn fee is 0.3%
+        yOutput = (xInputWithFee * yReserves) / (xReserves + xInputWithFee);
+        return yOutput;
     }
 
-    function getLiquidity(address lp) public view returns (uint256 lpLiquidity) {
-        // Your code here...
+    function getLiquidity(
+        address lp
+    ) public view returns (uint256 lpLiquidity) {
+        return liquidity[lp];
     }
 
     function ethToToken() public payable returns (uint256 tokenOutput) {
-        // Your code here...
+        if (msg.value == 0) revert InvalidEthAmount();
+        uint256 ethInput = msg.value;
+
+        uint256 tokenReserve = token.balanceOf(address(this));
+        uint256 ethReserve = address(this).balance - msg.value;
+        tokenOutput = price(ethInput, ethReserve, tokenReserve);
+        if (tokenReserve < tokenOutput)
+            revert InsufficientTokenBalance(tokenReserve, tokenOutput);
+
+        (bool success) = token.transfer(msg.sender, tokenOutput);
+        if (!success) revert TokenTransferFailed();
+        emit EthToTokenSwap(msg.sender, tokenOutput, ethInput);
     }
 
     function tokenToEth(uint256 tokenInput) public returns (uint256 ethOutput) {
-        // Your code here...
+        if (tokenInput == 0) revert InvalidTokenAmount();
+        uint256 bal = token.balanceOf(msg.sender);
+        if (bal < tokenInput) revert InsufficientTokenBalance(bal, tokenInput);
+        uint256 allow = token.allowance(msg.sender, address(this));
+        if (allow < tokenInput)
+            revert InsufficientTokenAllowance(allow, tokenInput);
+        uint256 tokenReserve = token.balanceOf(address(this));
+        ethOutput = price(tokenInput, tokenReserve, address(this).balance);
+        if (!token.transferFrom(msg.sender, address(this), tokenInput))
+            revert TokenTransferFailed();
+        (bool sent, ) = msg.sender.call{value: ethOutput}("");
+        if (!sent) revert EthTransferFailed(msg.sender, ethOutput);
+        emit TokenToEthSwap(msg.sender, tokenInput, ethOutput);
+        return ethOutput;
     }
 
     function deposit() public payable returns (uint256 tokensDeposited) {
-        // Your code here...
+        if (msg.value == 0) revert InvalidEthAmount();
+        uint256 allow = token.allowance(msg.sender, address(this));
+
+        uint256 ethInput = msg.value;
+        uint256 tokenReserve = token.balanceOf(address(this));
+        uint256 ethReserve = address(this).balance - msg.value;
+
+        //caluclated using LP minted = (amount added / reserve before deposit) × total LP supply coul also be LP minted = ΔTOKEN × S / RTOKEN since both eth and tokens are both added in correct ratio
+        uint256 liquidityMinted = (ethInput * totalLiquidity) / ethReserve;
+
+        totalLiquidity += liquidityMinted;
+        liquidity[msg.sender] += liquidityMinted;
+        tokensDeposited = (ethInput * tokenReserve) / ethReserve;
+
+        if (allow < tokensDeposited)
+            revert InsufficientTokenAllowance(allow, tokensDeposited);
+
+        if (!token.transferFrom(msg.sender, address(this), tokensDeposited))
+            revert TokenTransferFailed();
+        emit LiquidityProvided(
+            msg.sender,
+            liquidityMinted,
+            ethInput,
+            tokensDeposited
+        );
     }
 
-    function withdraw(uint256 amount) public returns (uint256 ethAmount, uint256 tokenAmount) {
-        // Your code here...
+    function withdraw(
+        uint256 amount
+    ) public returns (uint256 ethAmount, uint256 tokenAmount) {
+        if (amount > liquidity[msg.sender]) {
+            revert InsufficientLiquidity(liquidity[msg.sender], amount);
+        }
+        uint256 tokenReserve = token.balanceOf(address(this));
+        uint256 ethReserve = address(this).balance ;
+        ethAmount = (amount * ethReserve) / totalLiquidity;
+        tokenAmount = (amount * tokenReserve) / totalLiquidity;
+        liquidity[msg.sender] -= amount;
+        totalLiquidity -= amount;
+        (bool success, ) = payable(msg.sender).call{value: ethAmount}("");
+
+        if (!success) revert EthTransferFailed(msg.sender, ethAmount);
+        if (!token.transfer(msg.sender, tokenAmount))
+            revert TokenTransferFailed();
+
+        emit LiquidityRemoved(msg.sender, amount, tokenAmount, ethAmount);
     }
 }
--- packages/foundry/script/DeployDEX.s.sol
+++ packages/foundry/script/DeployDEX.s.sol
@@ -8,16 +8,23 @@ import "./DeployHelpers.s.sol";
 contract DeployDEX is ScaffoldETHDeploy {
     function run() external ScaffoldEthDeployerRunner {
         Balloons balloons = new Balloons();
-        console.logString(string.concat("Balloons deployed at: ", vm.toString(address(balloons))));
+        console.logString(
+            string.concat(
+                "Balloons deployed at: ",
+                vm.toString(address(balloons))
+            )
+        );
 
         DEX dex = new DEX(address(balloons));
-        console.logString(string.concat("DEX deployed at: ", vm.toString(address(dex))));
+        console.logString(
+            string.concat("DEX deployed at: ", vm.toString(address(dex)))
+        );
 
         // CHECKPOINT 2: Uncomment and replace YOUR_FRONTEND_ADDRESS to send 10 BAL to your frontend
-        // balloons.transfer(YOUR_FRONTEND_ADDRESS, 10 ether);
+        balloons.transfer(0xeAFcE79d3f1c329704D85179eB42291214F9B0C9, 10 ether);
 
         // CHECKPOINT 3: Uncomment below to seed initial liquidity after implementing init()
-        // balloons.approve(address(dex), 100 ether);
-        // dex.init{value: 5 ether}(5 ether);
+        balloons.approve(address(dex), 100 ether);
+        dex.init{value: 5 ether}(5 ether);
     }
 }
